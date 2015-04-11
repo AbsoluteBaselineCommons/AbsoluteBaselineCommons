3d89b278ced53f87a6f87b8573d9ea7c5e991c19
JustGregory <gregory.cheyney@gmail.com>
2015-04-11 14:25:45 -0500

    Tweaks in Javadoc.
    Only a couple minor corrections.

--- abc-constraints/src/main/java/abc/constraint/ConstrainLong.java
+++ abc-constraints/src/main/java/abc/constraint/ConstrainLong.java
@@ -1,16 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abc.constraint;
 
 
 /**
- *
+ * <h2>ConstrainLong</h2>
+ * Constrain a long value. This class has static methods to constrain a {@link Long} value in some specific ways.
  * @author Gregory
  */
 public class ConstrainLong {
+
+  private ConstrainLong() {
+  }
 
   /**
    * A public method to get whether a value is within a particular range.
